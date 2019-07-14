46cb3cc60e301a20cc6a289c9ed7809d19b0e99d
ahmad <ahmadbahaidarah@gmail.com>
2019-07-14 12:18:00 +0300

    end of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -28,8 +28,7 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-
-
+                    
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("User already exists with the same name!");
@@ -44,6 +43,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+        
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
